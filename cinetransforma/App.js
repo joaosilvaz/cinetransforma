5f428f89be04a49570d3d3422c16073aa40f74e1
joaosilvaz <joaovitoraz31@gmail.com>
2025-04-23 15:45:51 -0300

    [João] - Adicionando a tela de Sobre nós

--- cinetransforma/App.js
+++ cinetransforma/App.js
@@ -1,92 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/components/Home';
+import About from './src/components/About';
 
-export default function Home() {
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>🎬 CineTransforma</Text>
-      </View>
-
-      <ScrollView contentContainerStyle={styles.content}>
-        <Text style={styles.title}>Explicação do site</Text>
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id diam quis nunc varius ultrices eu a risus. Mauris ultrices quam sed rutrum molestie. Curabitur vitae justo turpis. Sed dignissim elit in orci varius, vitae vulputate odio luctus. Proin sit amet purus vestibulum, convallis nisi vitae, suscipit lacus.
-        </Text>
-        <Image
-          source={require('./assets/banner.png')}
-          style={styles.image}
-          resizeMode="contain"
-        />
-      </ScrollView>
-
-      <View style={styles.footer}>
-        <View style={styles.footerItem}>
-          <Ionicons name="color-palette-outline" size={24} color="red" />
-          <Text style={styles.footerText}>Temas</Text>
-        </View>
-        <View style={styles.footerItem}>
-          <Ionicons name="home-outline" size={24} color="red" />
-          <Text style={styles.footerText}>Home</Text>
-        </View>
-        <View style={styles.footerItem}>
-          <Ionicons name="information-circle-outline" size={24} color="red" />
-          <Text style={styles.footerText}>Sobre Nós</Text>
-        </View>
-      </View>
-    </View>
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="About" component={About} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1a1a1a',
-  },
-  header: {
-    backgroundColor: '#000',
-    paddingVertical: 15,
-    alignItems: 'center',
-  },
-  headerTitle: {
-    color: 'red',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  content: {
-    padding: 28,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    color: '#fff',
-    marginBottom: 10,
-    fontWeight: 'bold',
-  },
-  text: {
-    color: '#ccc',
-    textAlign: 'justify',
-    marginBottom: 20,
-  },
-  image: {
-    width: 600,
-    height: 400,
-    borderRadius: 8,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 10,
-    borderTopWidth: 1,
-    borderColor: '#333',
-    backgroundColor: '#000',
-  },
-  footerItem: {
-    alignItems: 'center',
-  },
-  footerText: {
-    color: 'red',
-    fontSize: 12,
-  },
-});
---
+++ cinetransforma/src/components/Home.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function Home() {
+    const navigation = useNavigation();
+    
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>🎬 CineTransforma</Text>
+            </View>
+
+            <ScrollView contentContainerStyle={styles.content}>
+                <Text style={styles.title}>Explicação do site</Text>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id diam quis nunc varius ultrices eu a risus. Mauris ultrices quam sed rutrum molestie. Curabitur vitae justo turpis. Sed dignissim elit in orci varius, vitae vulputate odio luctus. Proin sit amet purus vestibulum, convallis nisi vitae, suscipit lacus.
+                </Text>
+                <Image
+                    source={require('../../assets/banner.png')} 
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+            </ScrollView>
+
+            <View style={styles.footer}>
+                <View style={styles.footerItem}>
+                    <Ionicons name="film-outline" size={24} color="red" />
+                    <Text style={styles.footerText}>Temas</Text>
+                </View>
+                <View style={styles.footerItem}>
+                    <Ionicons name="home" size={24} color="red" />
+                    <Text style={styles.footerText}>Home</Text>
+                </View>
+                <View style={styles.footerItem}>
+                    <Ionicons name="information-circle-outline" size={24} color="red" onPress={() => navigation.navigate('About')}/>
+                    <Text style={styles.footerText}>Sobre Nós</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1a1a1a',
+    },
+    header: {
+        backgroundColor: '#000',
+        paddingVertical: 15,
+        alignItems: 'center',
+    },
+    headerTitle: {
+        color: 'red',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    content: {
+        padding: 28,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        color: '#fff',
+        marginBottom: 10,
+        fontWeight: 'bold',
+    },
+    text: {
+        color: '#ccc',
+        textAlign: 'justify',
+        marginBottom: 20,
+    },
+    image: {
+        width: 600,
+        height: 400,
+        borderRadius: 8,
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 10,
+        borderTopWidth: 1,
+        borderColor: '#333',
+        backgroundColor: '#000',
+    },
+    footerItem: {
+        alignItems: 'center',
+    },
+    footerText: {
+        color: 'red',
+        fontSize: 12,
+    },
+});
